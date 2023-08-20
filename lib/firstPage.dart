25eb8f8968cbdcec3a8fb2e5b65394f110690c37
yousef zidan <jouzidan111@gmail.com>
2023-08-20 12:02:02 +0300

    third

--- lib/firstPage.dart
+++ lib/firstPage.dart
@@ -21,7 +21,7 @@ class _QuizPageState extends State<QuizPage> {
       if(checkedAnswer != null){
         setState(() {
           quizNum++;
-          print(quizNum);
+          // print(quizNum);
           choosenAnswers.add(checkedAnswer);
           checkedAnswer = null;
         });
@@ -33,7 +33,7 @@ class _QuizPageState extends State<QuizPage> {
               title: const Text('You must choose answer', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
               content: const Text('Think about it, if you choose any answer There is chance you might get it right : )', style: TextStyle(color: Colors.white),),
               backgroundColor: barColor,
-              
+
               actions: [
                 TextButton(
                   onPressed: () {
@@ -49,9 +49,9 @@ class _QuizPageState extends State<QuizPage> {
     } else {
       quizNum = 0;
       Navigator.push(
-        context,
-         MaterialPageRoute(builder: (context) => endPage())
-        );
+          context,
+          MaterialPageRoute(builder: (context) => endPage())
+      );
     }
   }
 
@@ -59,8 +59,9 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const SizedBox(height: 65.0),
+          const SizedBox(height: 35.0),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
             child: Text(
@@ -78,9 +79,12 @@ class _QuizPageState extends State<QuizPage> {
 
           const SizedBox(height: 30.0),
           Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
             child: Text(
               questionsss[quizNum]['question'],
               overflow: TextOverflow.clip, // ellipsis
+              textAlign: TextAlign.center,
               style: const TextStyle(
                 fontSize: 22.0,
                 color: Colors.white,
@@ -89,10 +93,10 @@ class _QuizPageState extends State<QuizPage> {
           ),
 
           const SizedBox(height: 30.0),
-          ChoiceButton(label: 'A', text: questionsss[quizNum]['answers'][0]),
-          ChoiceButton(label: 'B', text: questionsss[quizNum]['answers'][1]),
-          ChoiceButton(label: 'C', text: questionsss[quizNum]['answers'][2]),
-          ChoiceButton(label: 'D', text: questionsss[quizNum]['answers'][3]),
+          ChoiceButton(label: 'A', text: questionsss[quizNum]['answers'][0], checked: false),
+          ChoiceButton(label: 'B', text: questionsss[quizNum]['answers'][1], checked: false),
+          ChoiceButton(label: 'C', text: questionsss[quizNum]['answers'][2], checked: false),
+          ChoiceButton(label: 'D', text: questionsss[quizNum]['answers'][3], checked: false),
 
           const SizedBox(height: 30.0),
           Container(
@@ -106,7 +110,7 @@ class _QuizPageState extends State<QuizPage> {
               //     MaterialPageRoute(builder: (context) =>secPage(),)
               //     );
               // }
-            ,
+              ,
 
               elevation: 5,
               shape: RoundedRectangleBorder(
@@ -115,7 +119,7 @@ class _QuizPageState extends State<QuizPage> {
               color: Color.fromRGBO(245, 90, 12,1),
               height: 60,
               minWidth: 100,
-              padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 80,vertical: 10),
               child: const Text('Next',style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w600),),
 
             ),
@@ -131,55 +135,74 @@ class _QuizPageState extends State<QuizPage> {
 class ChoiceButton extends StatelessWidget {
   final String label;
   final String text;
+  bool checked = false;
 
-  String ch ="";
+  // String ch ="";
 
-  ChoiceButton({super.key, required this.label, required this.text});
+  ChoiceButton({super.key, required this.label, required this.text, required this.checked});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 12.0),
+      padding:const EdgeInsets.symmetric(horizontal: 10,vertical: 0),
       child: Material(
         color: Colors.transparent,
         child: Container(
           decoration: BoxDecoration(
-            color: const Color(0xdff5eeff),
+            color: Color(0xdff5eeff),
             borderRadius: BorderRadius.circular(40.0),
           ),
           padding: const EdgeInsets.symmetric(horizontal: 1,vertical: 4),
-          child: Row(
-            //mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              MaterialButton( onPressed: (){
+          child: Container(
+              width: double.infinity, // or set a fixed width
+              child: MaterialButton(
+                // color: checked! ?  orangeOne :const Color.fromARGB(255, 197, 197, 197),
+                onPressed: (){
                   checkedAnswer = text;
+                  checked = true;
                   print(checkedAnswer);
                 },
-                elevation: 10,
-                color: orangeOne,
-                shape: CircleBorder(),
-                child: Text(
-                  label,
-                  style: const TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
+                child:Row(
+                  children: [
+                    MaterialButton(
+                      onPressed: (){
+                        checkedAnswer = text;
+                        checked = true;
+                        print(checkedAnswer);
+                      },
+                      minWidth: 5,
+                      // elevation: 10,
+                      color: checked ?  bodyColor : orangeOne,
+                      shape: CircleBorder(),
 
-              const SizedBox(width: 15.0),//between A and Paris
+                      child:Text(
+                          label,
+                          style: const TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                          ),
+                      ),
+                    ),
 
-              Container(
-                child:Text(
-                  text,
-                  style: const TextStyle(
-                      fontSize: 25.0,
-                      color: Color.fromRGBO(13, 19, 43,1),
-                      fontWeight: FontWeight.bold
-                  ),
+                    const SizedBox(width: 5.0),
+
+                    Expanded(
+                      child: Text(
+                        text,
+                        overflow: TextOverflow.visible,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromRGBO(13, 19, 43,1),
+                            fontWeight: FontWeight.normal
+                        ),
+                      ),
+                    ),
+
+                  ],
                 ),
               )
-            ],
           ),
         ),
       ),
